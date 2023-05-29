e4a4f9f4c54e0d8ebf10c78e52d727ddb2580e17
ninjaguydan <dthompson0190@gmail.com>
2023-05-29 07:42:02 -0500

    404 test

---
+++ src/Components/Pages/Error404/index.tsx
@@ -0,0 +1,20 @@
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import store from "../../../Redux/store";
+// CSS
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./css/Reset.css";
+import "./css/Style.css";
+import "./css/Login-Registration.css";
+import "./css/Search.css";
+import "./css/Profile.css";
+import "./css/Team.css";
+// Comps
+import Error from "./PageNotFound";
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Error />
+  </Provider>,
+  document.getElementById("error")
+);
